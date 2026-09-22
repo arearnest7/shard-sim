ec6e167b7e761629c4c09e535e2756643445c8c9
arearnest7 <arearnest7@tamu.edu>
2026-09-22 00:40:17 -0500

    fixed bugs with both arbiter and baseline variants that caused improper deallocation and reallocation to occur, resulting in stranded resource provisions and incorrect timelines; pending fixes for k-events, memory cost and peak (if needed)

--- scripts/simulate.py
+++ scripts/simulate.py
@@ -31,7 +31,7 @@ mu_sigma = {
 	"pileup": {"mu": 95.0, "sigma": 12.0}
 }
 
-total_time = 2000000 # ms
+total_time = 200000 #2000000 # ms
 
 instances = {}
 # workflow_id: {allocated_total: mem, shards: {shard_id: {in_process: request, allocated: mem, last_invoked: time, owner: function_name}}}
@@ -77,8 +77,7 @@ def main():
 			writer = csv.DictWriter(f, fieldnames=stats.keys())
 			writer.writeheader()
 			for incoming_request in requests:
-				#start_time = datetime.datetime.now()
-				while stats["time"] < float(incoming_request["time"]):
+				while stats["time"] < math.floor(float(incoming_request["time"])):
 					if str(stats["time"]) in next_event:
 						if args.scheduler == "arbiter":
 							for workflow_id, shard_ids in next_event[str(stats["time"])].items():
@@ -93,20 +92,21 @@ def main():
 											stats["used_" + instances[workflow_id]["shards"][shard_id]["owner"]] -= mem_shard
 											stats["alloc_total"] -= mem_shard
 											stats["alloc_" + instances[workflow_id]["shards"][shard_id]["owner"]] -= mem_shard
-											if instances[workflow_id]["shards"][shard_id]["owner"] == "pileup": # final function executed, shutting down (we know what the last function is beforehand)
+											if len(no_process[workflow_id]) == len(instances[workflow_id]["shards"]): # final function executed, shutting down (we know what the last function is beforehand)
 												to_delete = True
 											else:
 												instances[workflow_id]["shards"][shard_id]["process"] = None
 												no_process[workflow_id][shard_id] = 1
 										if to_delete:
 											k_events["container"]["delete"][workflow_id] += 1
+											no_process[workflow_id]
 											del instances[workflow_id]
 						else:
 							for function_name, instance_ids in next_event[str(stats["time"])].items():
 								for instance_id, targets in instance_ids.items():
 									for target, c in targets.items():
 										if instance_id in instances[function_name]:
-											if float(instances[function_name][instance_id]["process"][target]["time"]) + float(instances[function_name][instance_id]["process"][target]["latency"]) < stats["time"]: # request done, returning to idle
+											if target not in no_process[function_name][instance_id] and float(instances[function_name][instance_id]["process"][target]["time"]) + float(instances[function_name][instance_id]["process"][target]["latency"]) < stats["time"]: # request done, returning to idle
 												mem_process = float(instances[function_name][instance_id]["process"][target]["mem"])
 												stats["used_total"] -= mem_process
 												stats["used_" + function_name] -= mem_process
@@ -120,6 +120,10 @@ def main():
 													elif args.assignment == "exact":
 														allocated[instances[function_name][instance_id]["owner"][target]] -= mem_process
 														instances[function_name][instance_id]["allocated"] -= mem_process
+														stats["alloc_total"] -= mem_process
+														stats["alloc_" + function_name] -= mem_process
+														stats["idle_total"] -= mem_process
+														stats["idle_" + function_name] -= mem_process
 													if allocated[instances[function_name][instance_id]["owner"][target]] < 1:
 														del allocated[instances[function_name][instance_id]["owner"][target]]
 												if instance_id not in no_process[function_name]:
@@ -139,7 +143,6 @@ def main():
 						if stat_name != "time" and peak[stat_name] < stat:
 							peak[stat_name] = stat
 					if args.scheduler == "arbiter":
-						print(len(instances))
 						for workflow_id, instance in instances.items(): # has not been reclaimed and needs to be "billed"
 							memory_cost[workflow_id] += instance["allocated_total"]
 						writer.writerow(stats) # write simulation timeline
@@ -150,7 +153,6 @@ def main():
 								to_delete.append(workflow_id)
 						for workflow_id in to_delete:
 							del allocated[workflow_id]
-						print(len(allocated))
 						for workflow_id, alloc in allocated.items():
 							memory_cost[workflow_id] += alloc
 						writer.writerow(stats)
@@ -163,7 +165,6 @@ def main():
 						for m in ["create", "delete", "modify"]:
 							k_events[c][m][wf_id] = 0
 				function_name = incoming_request["request"].split("_")[2]
-				#print((datetime.datetime.now() - start_time).total_seconds()*1000000)
 				if args.scheduler == "arbiter":
 					if wf_id not in instances:
 						k_events["container"]["create"][wf_id] += 1
@@ -171,7 +172,8 @@ def main():
 						memory_cost[wf_id] = 0
 						no_process[wf_id] = {}
 					if len(no_process[wf_id]) > 0: # find pre-existing idle shard for incoming request
-						shard_id = no_process[wf_id].pop(next(iter(no_process[wf_id])))
+						shard_id = next(iter(no_process[wf_id]))
+						del no_process[wf_id][shard_id]
 						instances[wf_id]["shards"][shard_id]["process"] = incoming_request
 						instances[wf_id]["shards"][shard_id]["last_invoked"] = float(incoming_request["time"])
 						instances[wf_id]["shards"][shard_id]["owner"] = function_name
@@ -207,51 +209,45 @@ def main():
 					if wf_id not in allocated:
 						allocated[wf_id] = 0
 						memory_cost[wf_id] = 0
-					#start_time = datetime.datetime.now()
 					if len(no_process[function_name]) > 0: # find pre-existing idle instance for incoming request
 						instance_id = next(iter(no_process[function_name]))
 						target = next(iter(no_process[function_name][instance_id]))
-						prev_request = None
-						if target in instances[function_name][instance_id]["process"]:
-							prev_request = instances[function_name][instance_id]["process"][target]
 						instances[function_name][instance_id]["process"][target] = incoming_request
 						instances[function_name][instance_id]["last_invoked"] = float(incoming_request["time"])
 						instances[function_name][instance_id]["owner"][target] = wf_id
-						if args.assignment == "standard" and prev_request is None:
+						if args.assignment == "standard":
 							allocated[instances[function_name][instance_id]["owner"][target]] += container_alloc[function_name] / args.target
+							stats["idle_total"] -= float(incoming_request["mem"])
+							stats["idle_" + function_name] -= float(incoming_request["mem"])
 						elif args.assignment == "exact":
-							if prev_request != None:
-								instances[function_name][instance_id]["allocated"] -= float(prev_request["mem"])
-								allocated[instances[function_name][instance_id]["owner"][target]] -= float(prev_request["mem"])
 							allocated[instances[function_name][instance_id]["owner"][target]] += float(incoming_request["mem"])
 							instances[function_name][instance_id]["allocated"] += float(incoming_request["mem"])
+							stats["alloc_total"] += float(incoming_request["mem"])
+							stats["alloc_" + function_name] += float(incoming_request["mem"])
 							k_events["container"]["modify"][wf_id] += 1
 						stats["used_total"] += float(incoming_request["mem"])
 						stats["used_" + function_name] += float(incoming_request["mem"])
-						stats["idle_total"] -= float(incoming_request["mem"])
-						stats["idle_" + function_name] -= float(incoming_request["mem"])
-						next_finish = str(int(math.ceil(float(instances[function_name][instance_id]["process"][target]["time"]) + float(instances[function_name][instance_id]["process"][target]["latency"]))))
-						next_ttl = str(int(math.ceil(instances[function_name][instance_id]["last_invoked"] + args.ttl)))
+						next_finish = int(math.ceil(float(instances[function_name][instance_id]["process"][target]["time"]) + float(instances[function_name][instance_id]["process"][target]["latency"])))
+						next_ttl = int(math.ceil(instances[function_name][instance_id]["last_invoked"] + args.ttl))
 						if next_ttl < next_finish:
 							next_ttl = next_finish
-						if next_finish not in next_event:
-							next_event[next_finish] = {}
-						if function_name not in next_event[next_finish]:
-							next_event[next_finish][function_name] = {}
-						if instance_id not in next_event[next_finish][function_name]:
-							next_event[next_finish][function_name][instance_id] = {}
-						if next_ttl not in next_event:
-							next_event[next_ttl] = {}
-						if function_name not in next_event[next_ttl]:
-							next_event[next_ttl][function_name] = {}
-						if instance_id not in next_event[next_ttl][function_name]:
-							next_event[next_ttl][function_name][instance_id] = {}
-						next_event[next_finish][function_name][instance_id][target] = 1
-						next_event[next_ttl][function_name][instance_id][target] = 1
+						if str(next_finish) not in next_event:
+							next_event[str(next_finish)] = {}
+						if function_name not in next_event[str(next_finish)]:
+							next_event[str(next_finish)][function_name] = {}
+						if instance_id not in next_event[str(next_finish)][function_name]:
+							next_event[str(next_finish)][function_name][instance_id] = {}
+						if str(next_ttl) not in next_event:
+							next_event[str(next_ttl)] = {}
+						if function_name not in next_event[str(next_ttl)]:
+							next_event[str(next_ttl)][function_name] = {}
+						if instance_id not in next_event[str(next_ttl)][function_name]:
+							next_event[str(next_ttl)][function_name][instance_id] = {}
+						next_event[str(next_finish)][function_name][instance_id][target] = 1
+						next_event[str(next_ttl)][function_name][instance_id][target] = 1
 						del no_process[function_name][instance_id][target]
 						if len(no_process[function_name][instance_id]) == 0:
 							del no_process[function_name][instance_id]
-						#print((datetime.datetime.now() - start_time).total_seconds()*1000000)
 					else: # create new instance for incoming request
 						k_events["container"]["create"][wf_id] += 1
 						mem_alloc = container_alloc[function_name]
@@ -271,30 +267,29 @@ def main():
 							allocated[instances[function_name][instance_id]["owner"]["0"]] += float(incoming_request["mem"])
 						stats["used_total"] += float(incoming_request["mem"])
 						stats["used_" + function_name] += float(incoming_request["mem"])
-						stats["idle_total"] += container_alloc[function_name] - float(incoming_request["mem"])
-						stats["idle_" + function_name] += container_alloc[function_name] - float(incoming_request["mem"])
-						stats["alloc_total"] += container_alloc[function_name]
-						stats["alloc_" + function_name] += container_alloc[function_name]
-						next_finish = str(int(math.ceil(float(instances[function_name][instance_id]["process"]["0"]["time"]) + float(instances[function_name][instance_id]["process"]["0"]["latency"]))))
-						next_ttl = str(int(math.ceil(instances[function_name][instance_id]["last_invoked"] + args.ttl)))
+						stats["idle_total"] += mem_alloc - float(incoming_request["mem"])
+						stats["idle_" + function_name] += mem_alloc - float(incoming_request["mem"])
+						stats["alloc_total"] += mem_alloc
+						stats["alloc_" + function_name] += mem_alloc
+						next_finish = int(math.ceil(float(instances[function_name][instance_id]["process"]["0"]["time"]) + float(instances[function_name][instance_id]["process"]["0"]["latency"])))
+						next_ttl = int(math.ceil(instances[function_name][instance_id]["last_invoked"] + args.ttl))
 						if next_ttl < next_finish:
 							next_ttl = next_finish
-						if next_finish not in next_event:
-							next_event[next_finish] = {}
-						if function_name not in next_event[next_finish]:
-							next_event[next_finish][function_name] = {}
-						if instance_id not in next_event[next_finish][function_name]:
-							next_event[next_finish][function_name][instance_id] = {}
-						if next_ttl not in next_event:
-							next_event[next_ttl] = {}
-						if function_name not in next_event[next_ttl]:
-							next_event[next_ttl][function_name] = {}
-						if instance_id not in next_event[next_ttl][function_name]:
-							next_event[next_ttl][function_name][instance_id] = {}
-						next_event[next_finish][function_name][instance_id]["0"] = 1
-						next_event[next_ttl][function_name][instance_id]["0"] = 1
+						if str(next_finish) not in next_event:
+							next_event[str(next_finish)] = {}
+						if function_name not in next_event[str(next_finish)]:
+							next_event[str(next_finish)][function_name] = {}
+						if instance_id not in next_event[str(next_finish)][function_name]:
+							next_event[str(next_finish)][function_name][instance_id] = {}
+						if str(next_ttl) not in next_event:
+							next_event[str(next_ttl)] = {}
+						if function_name not in next_event[str(next_ttl)]:
+							next_event[str(next_ttl)][function_name] = {}
+						if instance_id not in next_event[str(next_ttl)][function_name]:
+							next_event[str(next_ttl)][function_name][instance_id] = {}
+						next_event[str(next_finish)][function_name][instance_id]["0"] = 1
+						next_event[str(next_ttl)][function_name][instance_id]["0"] = 1
 						total_deployed[function_name] += 1
-						#print((datetime.datetime.now() - start_time).total_seconds()*1000000)
 				for stat_name, stat in stats.items(): # update the peak
 					if stat_name != "time" and peak[stat_name] < stat:
 						peak[stat_name] = stat
@@ -311,15 +306,13 @@ def main():
 										instances[workflow_id]["shards"][shard_id]["allocated"] = 0 # in practice this shrinks/expands to the next stage, but its a linear trace and I am not rebuilding the topology graphs as inputs (16+GB in memory); its corrected by the search for pre-existing in the next block
 										stats["used_total"] -= mem_shard
 										stats["used_" + instances[workflow_id]["shards"][shard_id]["owner"]] -= mem_shard
-										stats["idle_total"] += mem_shard - mem_shard
-										stats["idle_" + instances[workflow_id]["shards"][shard_id]["owner"]] += mem_shard - mem_shard
 										stats["alloc_total"] -= mem_shard
 										stats["alloc_" + instances[workflow_id]["shards"][shard_id]["owner"]] -= mem_shard
-										if instances[workflow_id]["shards"][shard_id]["owner"] == "pileup": # final function executed, shutting down (we know what the last function is beforehand)
+										if len(no_process[workflow_id]) == len(instances[workflow_id]["shards"]): # final function executed, shutting down (we know what the last function is beforehand)
 											to_delete = True
 										else:
 											instances[workflow_id]["shards"][shard_id]["process"] = None
-											no_process[workflow_id].append(shard_id)
+											no_process[workflow_id][shard_id] = 1
 									if to_delete:
 										k_events["container"]["delete"][workflow_id] += 1
 										del instances[workflow_id]
@@ -328,7 +321,7 @@ def main():
 							for instance_id, targets in instance_ids.items():
 								for target, c in targets.items():
 									if instance_id in instances[function_name]:
-										if float(instances[function_name][instance_id]["process"][target]["time"]) + float(instances[function_name][instance_id]["process"][target]["latency"]) < stats["time"]: # request done, returning to idle
+										if target not in no_process[function_name][instance_id] and float(instances[function_name][instance_id]["process"][target]["time"]) + float(instances[function_name][instance_id]["process"][target]["latency"]) < stats["time"]: # request done, returning to idle
 											mem_process = float(instances[function_name][instance_id]["process"][target]["mem"])
 											stats["used_total"] -= mem_process
 											stats["used_" + function_name] -= mem_process
@@ -341,6 +334,11 @@ def main():
 													allocated[instances[function_name][instance_id]["owner"][target]] -= container_alloc[function_name] / args.target
 												elif args.assignment == "exact":
 													allocated[instances[function_name][instance_id]["owner"][target]] -= mem_process
+													instances[function_name][instance_id]["allocated"] -= mem_process
+													stats["alloc_total"] -= mem_process
+													stats["alloc_" + function_name] -= mem_process
+													stats["idle_total"] -= mem_process
+													stats["idle_" + function_name] -= mem_process
 												if allocated[instances[function_name][instance_id]["owner"][target]] < 1:
 													del allocated[instances[function_name][instance_id]["owner"][target]]
 											if instance_id not in no_process[function_name]:
@@ -360,7 +358,6 @@ def main():
 					if stat_name != "time" and peak[stat_name] < stat:
 						peak[stat_name] = stat
 				if args.scheduler == "arbiter":
-					print(len(instances))
 					for workflow_id, instance in instances.items(): # has not been reclaimed and needs to be "billed"
 						memory_cost[workflow_id] += instance["allocated_total"]
 					writer.writerow(stats) # write simulation timeline
@@ -371,7 +368,6 @@ def main():
 							to_delete.append(workflow_id)
 					for workflow_id in to_delete:
 						del allocated[workflow_id]
-					print(len(allocated))
 					for workflow_id, alloc in allocated.items():
 						memory_cost[workflow_id] += alloc
 					writer.writerow(stats)
